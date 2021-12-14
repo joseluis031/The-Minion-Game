e7a59c3cc403828d5734ccfe03ce1f92943e00f5
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-12-14 10:40:30 +0100

    creo la funcion y defino variables

---
+++ miniom.py
@@ -0,0 +1,6 @@
+def miniom_game(palabra): #creo una funcion
+    marcador_Stuart = 0   #creo los marcadores de los 2 jugadores
+    marcador_Kevin = 0
+    vocales = 'AEIOUaeiou' # defino la variable vocales
+    p = len(palabra)      # defino una variable que lo que va a hacer es leerme la palabra que introduzca
+    
